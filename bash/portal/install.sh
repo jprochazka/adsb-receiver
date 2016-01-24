f2f6a4113c3642ac131df4f11fece6672f06092d
Joe Prochazka <joe@debian.Home>
2016-01-23 20:53:27 -0500

    Configure dump978 map.

--- bash/portal/install.sh
+++ bash/portal/install.sh
@@ -66,6 +66,8 @@ echo -e "\033[37m"
 CheckPackage cron
 CheckPackage collectd
 CheckPackage rrdtool
+CheckPackage lighttpd
+CheckPackage php5-cgi
 
 ## SETUP THE PORTAL WEBSITE
 
@@ -88,6 +90,64 @@ fi
 echo -e "\033[33m"
 echo -e "Placing portal files in Lighttpd's root directory...\033[37m"
 sudo cp -R ${HTMLDIR}/* ${DOCUMENTROOT}
+
+## CHECK IF DUMP978 HAS BEEN BUILT
+
+if [ -f $BUILDDIR/dump978/dump978 ] && [ -f $BUILDDIR/dump978/uat2text ] && [ -f $BUILDDIR/dump978/uat2esnt ] && [ -f $BUILDDIR/dump978/uat2json ]; then
+    echo -e "\033[33m"
+    echo -e "Configuring dump978 map...\033[37m"
+    # Check if the heywhatsthis.com range position file has already been downloaded.
+    if [ ! -f ${HTMLDIR}/dump978/upintheair.json ]; then
+        # Check if a heywhatsthat.com range file exists in the dump1090 HTML folder.
+        if [ -f /usr/share/dump1090-mutability/html/upintheair.json ]; then
+            echo -e "\033[33m"
+            echo -e "Copying heywhatsthat.com range file from dump1090 installation...\033[37m"
+            sudo cp /usr/share/dump1090-mutability/html/upintheair.json ${HTMLDIR}/dump978/
+        else
+            echo -e "\033[33m"
+            echo "The dump978 map is able to display terrain limit rings using data obtained"
+            echo "from the website http://www.heywhatsthat.com. Some work will be required on your"
+            echo "part including visiting http://www.heywhatsthat.com and generating a new"
+            echo "panorama set to your location."
+            echo -e "\033[37m"
+            read -p "Do you wish to add terrain limit rings to the dump1090 map? [Y/n] " ADDTERRAINRINGS
+
+            if [[ ! $ADDTERRAINRINGS =~ ^[Nn]$ ]]; then 
+                echo -e "\033[31m"
+                echo "READ THE FOLLOWING INSTRUCTION CAREFULLY!"
+                echo -e "\033[33m"
+                echo "To set up terrain limit rings you will need to first generate a panorama on the website"
+                echo "heywhatsthat.com. To do so visit the following URL:"
+                echo ""
+                echo "  http://www.heywhatsthat.com"
+                echo ""
+                echo "Once the webpage has loaded click on the tab titled New panorama. Fill out the required"
+                echo "information in the form to the left of the map."
+                echo ""
+                echo "After submitting the form your request will be put into a queue to be generated shortly."
+                echo "You will be informed when the generation of your panorama has been completed."
+                echo ""
+                echo "Once generated visit your newly created panorama. Near the top left of the page you will"
+                echo "see a URL displayed which will point you to your newly created panorama. Within this URL's"
+                echo "query string you will see ?view=XXXXXXXX where XXXXXXXX is the identifier for this panorama."
+                echo "Enter below the letters and numbers making up the view identifier displayed there."
+                echo ""
+                echo "Positions for terrain rings for both 10,000 and 40,000 feet will be downloaded by this"
+                echo "script once the panorama has been generated and you are ready to continue."
+                echo -e "\033[37m"
+                read -p "Your heywhatsthat.com view identifier: " HEYWHATSTHATVIEWID
+                read -e -p "First ring altitude in meters (default 3048 meters or 10000 feet): " -i "3048" HEYWHATSTHATRINGONE
+                read -e -p "Second ring altitude in meters (default 12192 meters or 40000 feet): " -i "12192" HEYWHATSTHATRINGTWO
+
+                # Download the generated panoramas JSON data.
+                echo -e "\033[33m"
+                echo "Downloading JSON data pertaining to the panorama ID you supplied..."
+                echo -e "\033[37m"
+                sudo wget -O ${HTMLDIR}/dump978/upintheair.json "http://www.heywhatsthat.com/api/upintheair.json?id=${HEYWHATSTHATVIEWID}&refraction=0.25&alts=$HEYWHATSTHATRINGONE,$HEYWHATSTHATRINGTWO"
+            fi
+        fi
+    fi
+fi
 
 echo -e "\033[33m"
 echo -e "Setting permissions on data files...\033[37m"
@@ -112,9 +172,17 @@ $HTTP["remoteip"] !~ "127.0.0.1" {
 EOF
 
 echo -e "\033[33m"
-echo "Restarting Lighttpd..."
-echo -e "\033[37m"
-sudo /etc/init.d/lighttpd restart
+echo -e "Enabling the Lighttpd fastcgi-php module...\033[37m"
+sudo lighty-enable-mod fastcgi-php
+
+echo -e "\033[33m"
+if pgrep "lighttpd" > /dev/null; then
+    echo "Reloading Lighttpd..."
+    sudo /etc/init.d/lighttpd force-reload
+else
+    echo "Starting Lighttpd..."
+    sudo /etc/init.d/lighttpd start
+fi
 
 ## SETUP COMPLETE
 
