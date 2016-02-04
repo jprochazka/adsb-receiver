060676296b9495407b2f2b87ca18c1d84b981328
Joe Prochazka <jprochazka@users.noreply.github.com>
2016-02-04 11:20:25 -0500

    Load jQuery before Bootstrap.

--- build/portal/html/templates/default/master.tpl.php
+++ build/portal/html/templates/default/master.tpl.php
@@ -70,8 +70,8 @@
                 </p>
             </div>
         </footer>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
         <script src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
         <script type="text/javascript">
             $('#<?php echo $linkId ?>').addClass("active");
         </script>
