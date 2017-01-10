4090b1eb238df1ae09594182a39369e8fa1d7f87
Romeo-Golf <Romeo-Golf@users.noreply.github.com>
2017-01-10 18:52:51 +0000

    Add hardware check functions

--- bash/functions.sh
+++ bash/functions.sh
@@ -117,3 +117,38 @@ function CleanLogFile {
     # Remove the "Press enter to continue..." lines from the log file.
     sed -i "/Press enter to continue.../d" $1
 }
+
+#################################################################################
+# Detect CPU Architecture.
+
+function Check_CPU () {
+    if [[ -z "${CPU_ARCHITECTURE}" ]] ; then
+        echo -en "\e[33m  Detecting CPU architecture...\e[97m"
+        CPU_ARCHITECTURE=`uname -m | tr -d "\n\r"`
+    fi
+}
+
+#################################################################################
+# Detect Platform.
+
+function Check_Platform () {
+    if [[ -z "${HARDWARE_PLATFORM}" ]] ; then
+        if [[ `egrep -c "^Hardware.*: BCM" /proc/cpuinfo` -gt 0 ]] ; then
+            HARDWARE_PLATFORM="RPI"
+        elif [[ `egrep -c "^Hardware.*: Allwinner sun4i/sun5i Families$" /proc/cpuinfo` -gt 0 ]] ; then
+            HARDWARE_PLATFORM="CHIP"
+        else
+            HARDWARE_PLATFORM="unknown"
+        fi
+    fi
+}
+
+#################################################################################
+# Detect Hardware Revision.
+
+function Check_Hardware () {
+    if [[ -z "${HARDWARE_REVISION}" ]] ; then
+        echo -en "\e[33m  Detecting Hardware revision...\e[97m"
+        HARDWARE_REVISION=`grep "^Revision" /proc/cpuinfo | awk '{print $3}'`
+    fi
+}
