d421c0c3d58a5dc93c666c9e8f4fb6f788555a1b
romeo-golf <romeo-golf@users.noreply.github.com>
2017-01-14 04:44:44 +0000

    AboveTustin - Update tests for variables in existing config

--- bash/extras/abovetustin.sh
+++ bash/extras/abovetustin.sh
@@ -194,7 +194,7 @@ fi
 if [[ "${RECEIVER_AUTOMATED_INSTALL}" = "false" ]] ; then
     TWITTER_ACCESS_TOKEN_TITLE="Twitter Access Token"
     while [[ -z ${TWITTER_ACCESS_TOKEN} ]] ; do
-        if [[ -f ${RECEIVER_BUILD_DIRECTORY}/AboveTustin/config.ini ]] ; then
+        if [[ `grep -c "^access_token =" ${RECEIVER_BUILD_DIRECTORY}/AboveTustin/config.ini` -gt 0 ]] ; then
             TWITTER_ACCESS_TOKEN=`GetConfig "access_token" "${RECEIVER_BUILD_DIRECTORY}/AboveTustin/config.ini"`
         fi
         TWITTER_ACCESS_TOKEN=$(whiptail --backtitle "${RECEIVER_PROJECT_TITLE}" --title "${TWITTER_ACCESS_TOKEN_TITLE}" --nocancel --inputbox "\nEnter your Twitter Access Token." 7 78 "${TWITTER_ACCESS_TOKEN}" 3>&1 1>&2 2>&3)
@@ -203,7 +203,7 @@ if [[ "${RECEIVER_AUTOMATED_INSTALL}" = "false" ]] ; then
     #
     TWITTER_ACCESS_TOKEN_SECRET_TITLE="Twitter Access Token Secret"
     while [[ -z ${TWITTER_ACCESS_TOKEN_SECRET} ]] ; do
-        if [[ -f ${RECEIVER_BUILD_DIRECTORY}/AboveTustin/config.ini ]] ; then
+        if [[ `grep -c "^access_token_secret =" ${RECEIVER_BUILD_DIRECTORY}/AboveTustin/config.ini` -gt 0 ]] ; then
             TWITTER_ACCESS_TOKEN_SECRET=`GetConfig "access_token_secret" "${RECEIVER_BUILD_DIRECTORY}/AboveTustin/config.ini"`
         fi
         TWITTER_ACCESS_TOKEN_SECRET=$(whiptail --backtitle "${RECEIVER_PROJECT_TITLE}" --title "${TWITTER_ACCESS_TOKEN_SECRET_TITLE}" --nocancel --inputbox "\nEnter your Twitter Access Token Secret." 7 78 "${TWITTER_ACCESS_TOKEN_SECRET}" 3>&1 1>&2 2>&3)
@@ -212,7 +212,7 @@ if [[ "${RECEIVER_AUTOMATED_INSTALL}" = "false" ]] ; then
     #
     TWITTER_CONSUMER_KEY_TITLE="Twitter Consumer Key"
     while [[ -z ${TWITTER_CONSUMER_KEY} ]] ; do
-        if [[ -f ${RECEIVER_BUILD_DIRECTORY}/AboveTustin/config.ini ]] ; then
+        if [[ `grep -c "^consumer_key =" ${RECEIVER_BUILD_DIRECTORY}/AboveTustin/config.ini` -gt 0 ]] ; then
             TWITTER_CONSUMER_KEY=`GetConfig "consumer_key" "${RECEIVER_BUILD_DIRECTORY}/AboveTustin/config.ini"`
         fi
         TWITTER_CONSUMER_KEY=$(whiptail --backtitle "${RECEIVER_PROJECT_TITLE}" --title "${TWITTER_CONSUMER_KEY_TITLE}" --nocancel --inputbox "\nEnter your Twitter Consumer Key." 7 78 "${TWITTER_CONSUMER_KEY}" 3>&1 1>&2 2>&3)
@@ -221,7 +221,7 @@ if [[ "${RECEIVER_AUTOMATED_INSTALL}" = "false" ]] ; then
     #
     TWITTER_CONSUMER_SECRET_TITLE="Twitter Consumer Secret"
     while [[ -z ${TWITTER_CONSUMER_SECRET} ]] ; do
-        if [[ -f ${RECEIVER_BUILD_DIRECTORY}/AboveTustin/config.ini ]] ; then
+        if [[ `grep -c "^consumer_secret =" ${RECEIVER_BUILD_DIRECTORY}/AboveTustin/config.ini` -gt 0 ]] ; then
             TWITTER_CONSUMER_SECRET=`GetConfig "consumer_secret" "${RECEIVER_BUILD_DIRECTORY}/AboveTustin/config.ini"`
         fi
         TWITTER_CONSUMER_SECRET=$(whiptail --backtitle "${RECEIVER_PROJECT_TITLE}" --title "${TWITTER_CONSUMER_SECRET_TITLE}" --nocancel --inputbox "\nEnter your Twitter Consumer Secret." 7 78 "${TWITTER_CONSUMER_SECRET}" 3>&1 1>&2 2>&3)
