6d2bd9525b5fd72729a339a0144aa23d40624a4b
romeo-golf <romeo-golf@users.noreply.github.com>
2017-01-17 01:24:05 +0000

    Generic feeder - prep work to harmonise this with ADS-B Exchange feeder script

--- bash/betas/genericfeeder.sh
+++ bash/betas/genericfeeder.sh
@@ -37,6 +37,27 @@ RECEIVER_ROOT_DIRECTORY="${PWD}"
 RECEIVER_BASH_DIRECTORY="${RECEIVER_ROOT_DIRECTORY}/bash"
 RECEIVER_BUILD_DIRECTORY="${RECEIVER_ROOT_DIRECTORY}/build"
 
+# Component pecific variables.
+
+FEEDER_BUILD_DIRECTORY="$PROJECTROOTDIRECTORY/build/generic-feeder"
+
+MLAT_CLIENT_BUILD_DIRECTORY="${RECEIVER_BUILD_DIRECTORY}/mlat-client"
+
+FEEDER_NAME=""
+FEEDER_BUILD_DIRECTORY="${RECEIVER_BUILD_DIRECTORY}/${FEEDER_NAME}"
+
+FEEDER_BEAST_DST_HOST=""
+FEEDER_BEAST_DST_PORT="30004"
+FEEDER_BEAST_SRC_HOST="127.0.0.1"
+FEEDER_BEAST_SRC_PORT="30005"
+
+FEEDER_MLAT_DST_HOST=""
+FEEDER_MLAT_DST_PORT="31090"
+FEEDER_MLAT_SRC_HOST="127.0.0.1"
+FEEDER_MLAT_SRC_PORT="30005"
+
+FEEDER_MLAT_RETURN_PORT="30104"
+
 ## INCLUDE EXTERNAL SCRIPTS
 
 source ${RECEIVER_BASH_DIRECTORY}/variables.sh
@@ -47,30 +68,12 @@ if [[ "${RECEIVER_AUTOMATED_INSTALL}" = "true" ]] ; then
     source ${RECEIVER_CONFIGURATION_FILE}
 fi
 
-# Component pecific variables.
-
-FEEDER_GENERIC_BUILD_DIRECTORY="$PROJECTROOTDIRECTORY/build/generic-feeder"
-
-MLAT_CLIENT_BUILD_DIRECTORY="${RECEIVER_BUILD_DIRECTORY}/mlat-client"
-
-FEEDER_GENERIC_NAME=""
-FEEDER_GENERIC_BUILD_DIRECTORY="${RECEIVER_BUILD_DIRECTORY}/${FEEDER_NAME}"
-
-FEEDER_GENERIC_BEAST_DST_HOST=""
-FEEDER_GENERIC_BEAST_DST_PORT="30004"
-FEEDER_GENERIC_BEAST_SRC_HOST="127.0.0.1"
-FEEDER_GENERIC_BEAST_SRC_PORT="30005"
-
-FEEDER_GENERIC_MLAT_DST_HOST=""
-FEEDER_GENERIC_MLAT_DST_PORT="31090"
-FEEDER_GENERIC_MLAT_SRC_HOST="127.0.0.1"
-FEEDER_GENERIC_MLAT_SRC_PORT="30005"
-FEEDER_GENERIC_MLAT_RETURN_PORT="30104"
-
 ## BEGIN SETUP
 
-clear
-echo -e "\n\e[91m  $ADSB_PROJECTTITLE"
+if [[ ${RECEIVER_AUTOMATED_INSTALL} = "false" ]] ; then
+    clear
+    echo -e "\n\e[91m  ${ADSB_PROJECTTITLE}"
+fi
 echo -e ""
 echo -e "\e[92m  Setting up the Generic Feeder..."
 echo -e "\e[93m----------------------------------------------------------------------------------------------------\e[96m"
@@ -91,7 +94,7 @@ if [ "$CONTINUESETUP" = 1 ]; then
     exit 1
 fi
 
-## CHECK FOR AND REMOVE  ANY OLD STYLE ADB-B EXCHANGE SETUPS IF ANY EXIST
+## CHECK FOR AND REMOVE ANY OLD STYLE ADB-B EXCHANGE SETUPS IF ANY EXIST
 
 ## CHECK FOR PREREQUISITE PACKAGES
 
@@ -114,39 +117,39 @@ CheckPackage netcat
 
 ### CONFIGURATION
 
-if [[ -n "${FEEDER_GENERIC_NAME}" ]] ; then
-    FEEDER_GENERIC_BEAST_SCRIPT="feeder-${FEEDER_GENERIC_NAME}-beast_maint.sh"
+if [[ -n "${FEEDER_NAME}" ]] ; then
+    FEEDER_BEAST_SCRIPT="feeder-${FEEDER_NAME}-beast_maint.sh"
 else
-    FEEDER_GENERIC_BEAST_SCRIPT="feeder-generic-beast_maint.sh"
+    FEEDER_BEAST_SCRIPT="feeder-generic-beast_maint.sh"
 fi
 
-# Create the adsbexchange directory in the build directory if it does not exist.
-echo -e "\e[94m  Checking for the generic feeder build directory...\e[97m"
-if [ ! -d "${FEEDER_GENERIC_BUILD_DIRECTORY}" ]; then
-    echo -e "\e[94m  Creating the generic feeder build directory...\e[97m"
-    mkdir ${FEEDER_GENERIC_BUILD_DIRECTORY}
-fi
+    # Create the adsbexchange directory in the build directory if it does not exist.
+    echo -e "\e[94m  Checking for the generic feeder build directory...\e[97m"
+    if [ ! -d "${FEEDER_BUILD_DIRECTORY}" ]; then
+        echo -e "\e[94m  Creating the generic feeder build directory...\e[97m"
+        mkdir ${FEEDER_BUILD_DIRECTORY}
+    fi
 
-echo -e "\e[94m  Creating the file ${FEEDER_GENERIC_BEAST_SCRIPT}...\e[97m"
-tee ${FEEDER_GENERIC_BUILD_DIRECTORY}/${FEEDER_GENERIC_BEAST_SCRIPT} > /dev/null <<EOF
+echo -e "\e[94m  Creating the file ${FEEDER_BEAST_SCRIPT}...\e[97m"
+tee ${FEEDER_BUILD_DIRECTORY}/${FEEDER_BEAST_SCRIPT} > /dev/null <<EOF
 #! /bin/bash
 while true
   do
-    /bin/nc ${FEEDER_GENERIC_BEAST_SRC_HOST} ${FEEDER_GENERIC_BEAST_SRC_PORT} | /bin/nc ${FEEDER_GENERIC_BEAST_DST_HOST} ${FEEDER_GENERIC_BEAST_DST_PORT}
+    /bin/nc ${FEEDER_BEAST_SRC_HOST} ${FEEDER_BEAST_SRC_PORT} | /bin/nc ${FEEDER_BEAST_DST_HOST} ${FEEDER_BEAST_DST_PORT}
     sleep 30
   done
 EOF
 
-echo -e "\e[94m  Setting file permissions for ${FEEDER_GENERIC_BEAST_SCRIPT}...\e[97m"
-sudo chmod +x ${FEEDER_GENERIC_BUILD_DIRECTORY}/${FEEDER_GENERIC_BEAST_SCRIPT}
+    echo -e "\e[94m  Setting file permissions for ${FEEDER_BEAST_SCRIPT}...\e[97m"
+    sudo chmod +x ${FEEDER_BUILD_DIRECTORY}/${FEEDER_BEAST_SCRIPT}
 
 
-echo -e "\e[94m  Checking if the netcat startup line is contained within the file /etc/rc.local...\e[97m"
-if ! grep -Fxq "${FEEDER_GENERIC_BUILD_DIRECTORY}/${FEEDER_GENERIC_BEAST_SCRIPT} &" /etc/rc.local; then
-    echo -e "\e[94m  Adding the netcat startup line to the file /etc/rc.local...\e[97m"
-    lnum=($(sed -n '/exit 0/=' /etc/rc.local))
-    ((lnum>0)) && sudo sed -i "${lnum[$((${#lnum[@]}-1))]}i ${FEEDER_GENERIC_BUILD_DIRECTORY}/${FEEDER_GENERIC_BEAST_SCRIPT} &\n" /etc/rc.local
-fi
+    echo -e "\e[94m  Checking if the netcat startup line is contained within the file /etc/rc.local...\e[97m"
+    if ! grep -Fxq "${FEEDER_BUILD_DIRECTORY}/${FEEDER_BEAST_SCRIPT} &" /etc/rc.local; then
+        echo -e "\e[94m  Adding the netcat startup line to the file /etc/rc.local...\e[97m"
+        lnum=($(sed -n '/exit 0/=' /etc/rc.local))
+        ((lnum>0)) && sudo sed -i "${lnum[$((${#lnum[@]}-1))]}i ${FEEDER_BUILD_DIRECTORY}/${FEEDER_BEAST_SCRIPT} &\n" /etc/rc.local
+    fi
 
 ## START THE MLAT-CLIENT AND NETCAT FEED
 
@@ -154,25 +157,25 @@ echo -e ""
 echo -e "\e[95m  Starting netcat feeds...\e[97m"
 echo -e ""
 
-# Kill any currently running instances of the ${FEEDER_GENERIC_BEAST_SCRIPT} script.
-echo -e "\e[94m  Checking for any running ${FEEDER_GENERIC_BEAST_SCRIPT} processes...\e[97m"
-PIDS=`ps -efww | grep -w "${FEEDER_GENERIC_BEAST_SCRIPT}" | awk -vpid=$$ '$2 != pid { print $2 }'`
-if [ ! -z "$PIDS" ]; then
-    echo -e "\e[94m  Killing any running ${FEEDER_GENERIC_BEAST_SCRIPT} processes...\e[97m"
-    sudo kill $PIDS
-    sudo kill -9 $PIDS
-fi
-PIDS=`ps -efww | grep -w "/bin/nc ${FEEDER_GENERIC_BEAST_DST_HOST}" | awk -vpid=$$ '$2 != pid { print $2 }'`
-if [ ! -z "$PIDS" ]; then
-    echo -e "\e[94m  Killing any running netcat processes...\e[97m"
-    sudo kill $PIDS
-    sudo kill -9 $PIDS
-fi
+    # Kill any currently running instances of the ${FEEDER_BEAST_SCRIPT} script.
+    echo -e "\e[94m  Checking for any running ${FEEDER_BEAST_SCRIPT} processes...\e[97m"
+    PIDS=`ps -efww | grep -w "${FEEDER_BEAST_SCRIPT}" | awk -vpid=$$ '$2 != pid { print $2 }'`
+    if [ ! -z "$PIDS" ]; then
+        echo -e "\e[94m  Killing any running ${FEEDER_BEAST_SCRIPT} processes...\e[97m"
+        sudo kill $PIDS
+        sudo kill -9 $PIDS
+    fi
+    PIDS=`ps -efww | grep -w "/bin/nc ${FEEDER_BEAST_DST_HOST}" | awk -vpid=$$ '$2 != pid { print $2 }'`
+    if [ ! -z "$PIDS" ]; then
+        echo -e "\e[94m  Killing any running netcat processes...\e[97m"
+        sudo kill $PIDS
+        sudo kill -9 $PIDS
+    fi
 
-# Kill any currently running instances of the adsbexchange-mlat_maint.sh script.
+    # Kill any currently running instances of the adsbexchange-mlat_maint.sh script.
 
-echo -e "\e[94m  Executing the ${FEEDER_GENERIC_BEAST_SCRIPT} script...\e[97m"
-sudo nohup ${FEEDER_GENERIC_BUILD_DIRECTORY}/${FEEDER_GENERIC_BEAST_SCRIPT} > /dev/null 2>&1 &
+    echo -e "\e[94m  Executing the ${FEEDER_BEAST_SCRIPT} script...\e[97m"
+    sudo nohup ${FEEDER_BUILD_DIRECTORY}/${FEEDER_BEAST_SCRIPT} > /dev/null 2>&1 &
 
 ### SETUP COMPLETE
 
