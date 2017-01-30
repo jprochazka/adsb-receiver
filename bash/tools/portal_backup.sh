11d113f5b3fbd3c3723c1fcfcc8d20cf603f0b19
romeo-golf <romeo-golf@users.noreply.github.com>
2017-01-30 03:13:04 +0000

    Widescale application of s/TEMPORARYDIRECTORY/TEMPORARY_DIRECTORY/g

--- bash/tools/portal_backup.sh
+++ bash/tools/portal_backup.sh
@@ -38,7 +38,7 @@
 BACKUPDATE=$(date +"%Y-%m-%d-%H%M%S")
 RECEIVER_ROOT_DIRECTORY="${PWD}"
 BACKUPSDIRECTORY="${RECEIVER_ROOT_DIRECTORY}/backups"
-TEMPORARYDIRECTORY="${RECEIVER_ROOT_DIRECTORY}/backup_${BACKUPDATE}"
+TEMPORARY_DIRECTORY="${RECEIVER_ROOT_DIRECTORY}/backup_${BACKUPDATE}"
 RAWDOCUMENTROOT=`/usr/sbin/lighttpd -f /etc/lighttpd/lighttpd.conf -p | grep server.document-root`
 LIGHTTPDDOCUMENTROOT=`sed 's/.*"\(.*\)"[^"]*$/\1/' <<< ${RAWDOCUMENTROOT}`
 
@@ -90,49 +90,49 @@ if [[ ! -d "${BACKUPSDIRECTORY}" ]] ; then
 fi
 
 # Check that the temporary directory exists.
-echo -e "\e[94m  Checking that the directory ${TEMPORARYDIRECTORY} exists...\e[97m"
-if [[ ! -d "${TEMPORARYDIRECTORY}" ]] ; then
+echo -e "\e[94m  Checking that the directory ${TEMPORARY_DIRECTORY} exists...\e[97m"
+if [[ ! -d "${TEMPORARY_DIRECTORY}" ]] ; then
     # Create the tmp directory.
-    echo -e "\e[94m  Creating the directory ${TEMPORARYDIRECTORY}...\e[97m"
-    mkdir -vp ${TEMPORARYDIRECTORY}
+    echo -e "\e[94m  Creating the directory ${TEMPORARY_DIRECTORY}...\e[97m"
+    mkdir -vp ${TEMPORARY_DIRECTORY}
 fi
 
 ## BACKUP THE FILES COMMON TO ALL PORTAL INSTALLATION SCENARIOS
 
 # Copy the collectd round robin database files to the temporary directory.
-echo -e "\e[94m  Checking that the directory ${TEMPORARYDIRECTORY}/var/lib/collectd/rrd/ exists...\e[97m"
-if [[ ! -d "${TEMPORARYDIRECTORY}/var/lib/collectd/rrd/" ]] ; then
-    mkdir -vp ${TEMPORARYDIRECTORY}/var/lib/collectd/rrd/
+echo -e "\e[94m  Checking that the directory ${TEMPORARY_DIRECTORY}/var/lib/collectd/rrd/ exists...\e[97m"
+if [[ ! -d "${TEMPORARY_DIRECTORY}/var/lib/collectd/rrd/" ]] ; then
+    mkdir -vp ${TEMPORARY_DIRECTORY}/var/lib/collectd/rrd/
 fi
 echo -e "\e[94m  Backing up the directory /var/lib/collectd/rrd/...\e[97m"
-sudo cp -R /var/lib/collectd/rrd/ ${TEMPORARYDIRECTORY}/var/lib/collectd/rrd/
+sudo cp -R /var/lib/collectd/rrd/ ${TEMPORARY_DIRECTORY}/var/lib/collectd/rrd/
 
 ## BACKUP PORTAL USING LITE FEATURES AND XML FILES
 
 if [[ "${ADVANCED}" = "FALSE" ]] ; then
     # Copy the portal XML data files to the temporary directory.
-    echo -e "\e[94m  Checking that the directory ${TEMPORARYDIRECTORY}/var/www/html/data/ exists...\e[97m"
-    if [[ ! -d "${TEMPORARYDIRECTORY}/var/www/html/data/" ]] ; then
-        mkdir -vp ${TEMPORARYDIRECTORY}/var/www/html/data/
+    echo -e "\e[94m  Checking that the directory ${TEMPORARY_DIRECTORY}/var/www/html/data/ exists...\e[97m"
+    if [[ ! -d "${TEMPORARY_DIRECTORY}/var/www/html/data/" ]] ; then
+        mkdir -vp ${TEMPORARY_DIRECTORY}/var/www/html/data/
     fi
-    echo -e "\e[94m  Backing up all XML data files to ${TEMPORARYDIRECTORY}/var/www/html/data/...\e[97m"
-    sudo cp -R /var/www/html/data/*.xml ${TEMPORARYDIRECTORY}/var/www/html/data/
+    echo -e "\e[94m  Backing up all XML data files to ${TEMPORARY_DIRECTORY}/var/www/html/data/...\e[97m"
+    sudo cp -R /var/www/html/data/*.xml ${TEMPORARY_DIRECTORY}/var/www/html/data/
 else
 
 ## BACKUP PORTAL USING ADVANCED FEATURES AND A SQLITE DATABASE
 
     if [[ "${DATABASEENGINE}" = "sqlite" ]] ; then
         # Copy the portal SQLite database file to the temporary directory.
-        echo -e "\e[94m  Backing up the SQLite database file to ${TEMPORARYDIRECTORY}/var/www/html/data/portal.sqlite...\e[97m"
-        sudo cp -R ${DATABASEPATH} ${TEMPORARYDIRECTORY}/var/www/html/data/portal.sqlite
+        echo -e "\e[94m  Backing up the SQLite database file to ${TEMPORARY_DIRECTORY}/var/www/html/data/portal.sqlite...\e[97m"
+        sudo cp -R ${DATABASEPATH} ${TEMPORARY_DIRECTORY}/var/www/html/data/portal.sqlite
     fi
 
 ## BACKUP PORTAL USING ADVANCED FEATURES AND A MYSQL DATABASE
 
     if [[ "${DATABASEENGINE}" = "mysql" ]] ; then
         # Dump the current MySQL database to a .sql text file.
-        echo -e "\e[94m  Dumping the MySQL database ${MYSQLDATABASE} to the file ${TEMPORARYDIRECTORY}/${MYSQLDATABASE}.sql...\e[97m"
-        mysqldump -u${MYSQLUSERNAME} -p${MYSQLPASSWORD} ${MYSQLDATABASE} > ${TEMPORARYDIRECTORY}/${MYSQLDATABASE}.sql
+        echo -e "\e[94m  Dumping the MySQL database ${MYSQLDATABASE} to the file ${TEMPORARY_DIRECTORY}/${MYSQLDATABASE}.sql...\e[97m"
+        mysqldump -u${MYSQLUSERNAME} -p${MYSQLPASSWORD} ${MYSQLDATABASE} > ${TEMPORARY_DIRECTORY}/${MYSQLDATABASE}.sql
     fi
 fi
 
@@ -141,12 +141,12 @@ fi
 # Create the backup archive.
 echo -e "\e[94m  Compressing the backed up files...\e[97m"
 echo -e ""
-tar -zcvf ${BACKUPSDIRECTORY}/adsb-receiver_data_${BACKUPDATE}.tar.gz ${TEMPORARYDIRECTORY}
+tar -zcvf ${BACKUPSDIRECTORY}/adsb-receiver_data_${BACKUPDATE}.tar.gz ${TEMPORARY_DIRECTORY}
 echo -e ""
 
 # Remove the temporary directory.
 echo -e "\e[94m  Removing the temporary backup directory...\e[97m"
-sudo rm -rf ${TEMPORARYDIRECTORY}
+sudo rm -rf ${TEMPORARY_DIRECTORY}
 
 ## BACKUP PROCESS COMPLETE
 
@@ -154,7 +154,7 @@ echo -e "\e[32m"
 echo -e "  BACKUP PROCESS COMPLETE\e[93m"
 echo -e ""
 echo -e "  An archive containing the data just backed up can be found at:"
-echo -e "  ${TEMPORARYDIRECTORY}/adsb-receiver_data_${BACKUPDATE}.tar.gz\e[97m"
+echo -e "  ${TEMPORARY_DIRECTORY}/adsb-receiver_data_${BACKUPDATE}.tar.gz\e[97m"
 echo -e ""
 
 echo -e "\e[93m  ------------------------------------------------------------------------------"
