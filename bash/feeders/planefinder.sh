1f5f501c3cbbc6712f38f06f24fa197a0b7e81a0
romeo-golf <romeo-golf@users.noreply.github.com>
2017-03-05 10:42:02 +0000

    PlaneFinder - Apply s/Plane Finder ADS-B Client/$COMPONENT_NAME/g

--- bash/feeders/planefinder.sh
+++ bash/feeders/planefinder.sh
@@ -38,7 +38,11 @@ RECEIVER_BASH_DIRECTORY="${RECEIVER_ROOT_DIRECTORY}/bash"
 RECEIVER_BUILD_DIRECTORY="${RECEIVER_ROOT_DIRECTORY}/build"
 
 # Component specific variables.
-COMPONENT_NAME="Plane Finder ADS-B Client"
+COMPONENT_NAME="PlaneFinder ADS-B Client"
+COMPONENT_PROVIDER="PlaneFinder"
+COMPONENT_PACKAGE_NAME="pfclient"
+COMPONENT_WEBSITE="https://planefinder.net/sharing/client"
+COMPONENT_GITHUB_URL=""
 COMPONENT_BUILD_DIRECTORY="${RECEIVER_BUILD_DIRECTORY}/planefinder"
 RECEIVER_IP_ADDRESS=`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
 
@@ -61,7 +65,7 @@ if [[ "${RECEIVER_AUTOMATED_INSTALL}" = "false" ]] ; then
     echo -e "\n\e[91m   ${RECEIVER_PROJECT_TITLE}"
 fi
 echo -e ""
-echo -e "\e[92m  Setting up the Plane Finder ADS-B Client..."
+echo -e "\e[92m  Setting up ${COMPONENT_NAME}..."
 echo -e ""
 echo -e "\e[93m  ------------------------------------------------------------------------------\e[96m"
 echo -e ""
@@ -71,21 +75,21 @@ echo -e ""
 # Confirm component installation.
 if [[ "${RECEIVER_AUTOMATED_INSTALL}" = "false" ]] ; then
     # Interactive install.
-    CONTINUE_SETUP=$(whiptail --backtitle "${RECEIVER_PROJECT_TITLE}" --title "Plane Finder ADS-B Client Setup" --yesno "The Plane Finder ADS-B Client is an easy and accurate way to share your ADS-B and MLAT data with Plane Finder. It comes with a beautiful user interface that helps you explore and interact with your data in realtime.\n\n  https://planefinder.net/sharing/client\n\nContinue setup by installing the Plane Finder ADS-B Client?" 13 78 3>&1 1>&2 2>&3)
+    CONTINUE_SETUP=$(whiptail --backtitle "${RECEIVER_PROJECT_TITLE}" --title "${COMPONENT_NAME} Setup" --yesno "The ${COMPONENT_NAME} is an easy and accurate way to share your ADS-B and MLAT data with Plane Finder. It comes with a beautiful user interface that helps you explore and interact with your data in realtime.\n\n  https://planefinder.net/sharing/client\n\nContinue setup by installing ${COMPONENT_NAME}?" 13 78 3>&1 1>&2 2>&3)
     if [[ ${CONTINUE_SETUP} -eq 1 ]] ; then
         # Setup has been halted by the user.
         echo -e "\e[91m  \e[5mINSTALLATION HALTED!\e[25m"
         echo -e "  Setup has been halted at the request of the user."
         echo -e ""
         echo -e "\e[93m  ------------------------------------------------------------------------------"
-        echo -e "\e[92m  Plane Finder ADS-B Client setup halted.\e[39m"
+        echo -e "\e[92m  ${COMPONENT_NAME} setup halted.\e[39m"
         echo -e ""
         read -p "Press enter to continue..." CONTINUE
         exit 1
     fi
 else
     # Warn that automated installation is not supported.
-    echo -e "\e[92m  Automated installation of this script is not yet supported...\e[39m"
+    echo -e "\e[92m  Automated installation of ${COMPONENT_NAME} not yet supported...\e[39m"
     echo -e ""
     exit 1
 fi
@@ -119,25 +123,25 @@ echo -e ""
 
 # Create the component build directory if it does not exist.
 if [[ ! -d "${COMPONENT_BUILD_DIRECTORY}" ]] ; then
-    echo -e "\e[94m  Creating the Plane Finder ADS-B Client build directory...\e[97m"
+    echo -e "\e[94m  Creating ${COMPONENT_NAME} build directory...\e[97m"
     mkdir -vp ${COMPONENT_BUILD_DIRECTORY}
 fi
 
 # Change to the comonent build directory.
 if [[ ! "${PWD}" = "${COMPONENT_BUILD_DIRECTORY}" ]] ; then
-    echo -e "\e[94m  Entering the Plane Finder ADS-B Client build directory...\e[97m"
+    echo -e "\e[94m  Entering ${COMPONENT_NAME} build directory...\e[97m"
     cd ${COMPONENT_BUILD_DIRECTORY} 2>&1
 fi
 
 # Download the appropriate package depending on the devices architecture.
 if [[ "${CPU_ARCHITECTURE}" = "armv7l" ]] || [[ "${CPU_ARCHITECTURE}" = "armv6l" ]] ; then
     # ARM achitecture detected.
-    echo -e "\e[94m  Downloading the Plane Finder ADS-B Client v${PLANEFINDER_CLIENT_VERSION_ARM} for ARM devices...\e[97m"
+    echo -e "\e[94m  Downloading ${COMPONENT_NAME} v${PLANEFINDER_CLIENT_VERSION_ARM} for ARM devices...\e[97m"
     echo -e ""
     wget --no-check-certificate https://client.planefinder.net/pfclient_${PLANEFINDER_CLIENT_VERSION_ARM}_armhf.deb -O ${COMPONENT_BUILD_DIRECTORY}/pfclient_${PLANEFINDER_CLIENT_VERSION_ARM}_armhf.deb
 else
     # Otherwise assume i386.
-    echo -e "\e[94m  Downloading the Plane Finder ADS-B Client v${PLANEFINDER_CLIENT_VERSION_I386} for i386 devices...\e[97m"
+    echo -e "\e[94m  Downloading ${COMPONENT_NAME} v${PLANEFINDER_CLIENT_VERSION_I386} for i386 devices...\e[97m"
     echo -e ""
     wget --no-check-certificate https://client.planefinder.net/pfclient_${PLANEFINDER_CLIENT_VERSION_I386}_i386.deb -O ${COMPONENT_BUILD_DIRECTORY}/pfclient_${PLANEFINDER_CLIENT_VERSION_I386}_i386.deb
 fi
@@ -145,18 +149,18 @@ fi
 ## INSTALL THE COMPONENT PACKAGE
 
 echo -e ""
-echo -e "\e[95m  Installing the Plane Finder ADS-B Client package...\e[97m"
+echo -e "\e[95m  Installing ${COMPONENT_NAME} package...\e[97m"
 echo -e ""
 
 # Install the proper package depending on the devices architecture.
 if [[ "${CPU_ARCHITECTURE}" = "armv7l" ]] || [[ "${CPU_ARCHITECTURE}" = "armv6l" ]] || [[ "${CPU_ARCHITECTURE}" = "aarch64" ]] ; then
     # ARM achitecture detected.
-    echo -e "\e[94m  Installing the Plane Finder ADS-B Client v${PLANEFINDER_CLIENT_VERSION_ARM} package for ARM devices...\e[97m"
+    echo -e "\e[94m  Installing ${COMPONENT_NAME} v${PLANEFINDER_CLIENT_VERSION_ARM} package for ARM devices...\e[97m"
     echo -e ""
     sudo dpkg -i ${COMPONENT_BUILD_DIRECTORY}/pfclient_${PLANEFINDER_CLIENT_VERSION_ARM}_armhf.deb 2>&1
 else
     # Otherwise assume i386.
-    echo -e "\e[94m  Installing the Plane Finder ADS-B Client v${PLANEFINDER_CLIENT_VERSION_I386} package for i386 devices...\e[97m"
+    echo -e "\e[94m  Installing ${COMPONENT_NAME} v${PLANEFINDER_CLIENT_VERSION_I386} package for i386 devices...\e[97m"
     if [[ `lsb_release -si` = "Debian" ]] ; then
         # Force architecture if this is Debian.
         echo -e "\e[94m  NOTE: dpkg executed with added flag --force-architecture.\e[97m"
@@ -185,7 +189,7 @@ if [[ -n "${CPU_ARCHITECTURE}" ]] ; then
         echo -e "\e[93mThe package \"pfclient\" could not be installed.\e[39m"
         echo -e ""
         echo -e "\e[93m  ------------------------------------------------------------------------------"
-        echo -e "\e[92m  Plane Finder ADS-B Client setup halted.\e[39m"
+        echo -e "\e[92m  ${COMPONENT_NAME} setup halted.\e[39m"
         echo -e ""
         if [[ "${RECEIVER_AUTOMATED_INSTALL}" = "false" ]] ; then
             read -p "Press enter to continue..." CONTINUE
@@ -201,7 +205,7 @@ if [[ -n "${CPU_ARCHITECTURE}" ]] ; then
         fi
 
         # Archive binary package.
-        echo -e "\e[94m  Moving the Plane Finder ADS-B Client binary package into the archive directory...\e[97m"
+        echo -e "\e[94m  Moving ${COMPONENT_NAME} binary package into the archive directory...\e[97m"
         echo -e ""
         mv -vf ${COMPONENT_BUILD_DIRECTORY}/pfclient_*.deb ${RECEIVER_BUILD_DIRECTORY}/package-archive 2>&1
         echo -e ""
@@ -210,7 +214,7 @@ fi
 
 ## DISPLAY FINAL SETUP INSTRUCTIONS WHICH CONNOT BE HANDLED BY THIS SCRIPT
 
-whiptail --backtitle "${RECEIVER_PROJECT_TITLE}" --title "Plane Finder ADS-B Client Setup Instructions" --msgbox "At this point the Plane Finder ADS-B Client should be installed and running howeverThis script is only capable of installing the Plane Finder ADS-B Client. There are stilla few steps left which you must manually do through the Plane Finder ADS-B Client at the following URL:\n\n  http://${RECEIVER_IP_ADDRESS}:30053\n\nThe follow the instructions supplied by the Plane Finder ADS-B Client.\n\nUse the following settings when asked for them.\n\nData Format: Beast\nTcp Address: 127.0.0.1\nTcp Port: 30005" 20 78
+whiptail --backtitle "${RECEIVER_PROJECT_TITLE}" --title "${COMPONENT_NAME} Setup Instructions" --msgbox "At this point ${COMPONENT_NAME} should be installed and running howeverThis script is only capable of installing ${COMPONENT_NAME}. There are stilla few steps left which you must manually do through ${COMPONENT_NAME} at the following URL:\n\n  http://${RECEIVER_IP_ADDRESS}:30053\n\nThe follow the instructions supplied by ${COMPONENT_NAME}.\n\nUse the following settings when asked for them.\n\nData Format: Beast\nTcp Address: 127.0.0.1\nTcp Port: 30005" 20 78
 
 ### SETUP COMPLETE
 
@@ -220,7 +224,7 @@ cd ${RECEIVER_ROOT_DIRECTORY} 2>&1
 
 echo -e ""
 echo -e "\e[93m  ------------------------------------------------------------------------------"
-echo -e "\e[92m  Plane Finder ADS-B Client setup is complete.\e[39m"
+echo -e "\e[92m  ${COMPONENT_NAME} setup is complete.\e[39m"
 echo -e ""
 if [[ "${RECEIVER_AUTOMATED_INSTALL}" = "false" ]] ; then
     read -p "Press enter to continue..." CONTINUE
