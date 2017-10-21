e912a8ae48e6c70ee04c058fc0df097e7ce1e2d6
Joe Prochazka <joe@Stretch.Home>
2017-10-21 15:17:31 -0400

    Added missing v2.6.0 portal upgrade file.

---
+++ build/portal/html/install/upgrade-v2.6.0.php
@@ -0,0 +1,48 @@
+<?php
+
+    /////////////////////////////////////////////////////////////////////////////////////
+    //                            ADS-B RECEIVER PORTAL                                //
+    // =============================================================================== //
+    // Copyright and Licensing Information:                                            //
+    //                                                                                 //
+    // The MIT License (MIT)                                                           //
+    //                                                                                 //
+    // Copyright (c) 2015-2017 Joseph A. Prochazka                                     //
+    //                                                                                 //
+    // Permission is hereby granted, free of charge, to any person obtaining a copy    //
+    // of this software and associated documentation files (the "Software"), to deal   //
+    // in the Software without restriction, including without limitation the rights    //
+    // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell       //
+    // copies of the Software, and to permit persons to whom the Software is           //
+    // furnished to do so, subject to the following conditions:                        //
+    //                                                                                 //
+    // The above copyright notice and this permission notice shall be included in all  //
+    // copies or substantial portions of the Software.                                 //
+    //                                                                                 //
+    // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR      //
+    // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,        //
+    // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE     //
+    // AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER          //
+    // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,   //
+    // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE   //
+    // SOFTWARE.                                                                       //
+    /////////////////////////////////////////////////////////////////////////////////////
+
+    class settings {
+            // Update the version and patch settings..
+            $common->updateSetting("version", "2.6.0");
+            $common->updateSetting("patch", "");
+
+            // The upgrade process completed successfully.
+            $results['success'] = TRUE;
+            $results['message'] = "Upgrade to v2.6.0 successful.";
+            return $results;
+
+        } catch(Exception $e) {
+            // Something went wrong during this upgrade process.
+            $results['success'] = FALSE;
+            $results['message'] = $e->getMessage();
+            return $results;
+        }
+    }
+?>
