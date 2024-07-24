a8fdf780dcf2f7133b83f7f9b0cc378dd6efbdb3
Joseph Prochazka <joe@swiftbyte.com>
2024-07-23 22:48:24 -0400

    Updated function to match merged changes.

--- bash/decoders/acarsdec.sh
+++ bash/decoders/acarsdec.sh
@@ -8,17 +8,19 @@ source $RECEIVER_BASH_DIRECTORY/variables.sh
 source $RECEIVER_BASH_DIRECTORY/functions.sh
 
 clear
-LogProjectName ${RECEIVER_PROJECT_TITLE}
-LogTitleHeading "Setting up the ACARSDEC decoder"
-LogTitleMessage "------------------------------------------------------------------------------"
-echo ""
-
-if ! whiptail --backtitle "${RECEIVER_PROJECT_TITLE}" --title "ACARSDEC decoder Setup" --yesno "Continue setup?" 13 78; then
-    LogAlertHeading "INSTALLATION HALTED"
-    LogAlertMessage "Setup has been halted at the request of the user"
+log_project_title ${RECEIVER_PROJECT_TITLE}
+log_title_heading "Setting up the ACARSDEC decoder"
+log_title_message "------------------------------------------------------------------------------"
+if ! whiptail --backtitle "${RECEIVER_PROJECT_TITLE}" \
+              --title "ACARSDEC decoder Setup" \
+              --yesno "Continue setup?" \
+              13 78; then
     echo ""
-    LogTitleMessage "------------------------------------------------------------------------------"
-    LogTitleHeading "ACARSDEC decoder setup halted"
+    log_alert_heading "INSTALLATION HALTED"
+    log_alert_message "Setup has been halted at the request of the user"
+    echo ""
+    log_title_message "------------------------------------------------------------------------------"
+    log_title_heading "ACARSDEC decoder setup halted"
     echo ""
     exit 1
 fi
@@ -31,36 +33,36 @@ fi
 
 ## CHECK FOR PREREQUISITE PACKAGES
 
-LogHeading "Installing packages needed to fulfill dependencies for FlightAware PiAware client"
+log_heading "Installing packages needed to fulfill dependencies for FlightAware PiAware client"
 
-CheckPackage cmake
-CheckPackage zlib1g-dev
-CheckPackage libxml2-dev
-CheckPackage libjansson-dev
-CheckPackage libusb-1.0-0-dev
-CheckPackage pkg-config
-CheckPackage libsndfile-dev
-CheckPackage libpaho-mqtt-dev
+check_package cmake
+check_package zlib1g-dev
+check_package libxml2-dev
+check_package libjansson-dev
+check_package libusb-1.0-0-dev
+check_package pkg-config
+check_package libsndfile-dev
+check_package libpaho-mqtt-dev
 
-CheckPackage librtlsdr-dev
-CheckPackage libairspy-dev
-CheckPackage libmirisdr-dev
+check_package librtlsdr-dev
+check_package libairspy-dev
+check_package libmirisdr-dev
 
 
 ## CLONE OR PULL THE LIBACARS GIT REPOSITORY
 
-LogHeading "Preparing the libacars Git repository"
+log_heading "Preparing the libacars Git repository"
 
 if [[ -d $RECEIVER_BUILD_DIRECTORY/libacars && -d $RECEIVER_BUILD_DIRECTORY/libacars/.git ]]; then
-    LogMessage "Entering the libacars git repository directory"
+    log_message "Entering the libacars git repository directory"
     cd $RECEIVER_BUILD_DIRECTORY/libacars
-    LogMessage "Updating the local libacars git repository"
+    log_message "Updating the local libacars git repository"
     echo ""
     git pull
 else
-    LogMessage "Entering the libacars build directory"
+    log_message "Entering the libacars build directory"
     cd $RECEIVER_BUILD_DIRECTORY
-    LogMessage "Cloning the libacars git repository locally"
+    log_message "Cloning the libacars git repository locally"
     echo ""
     git clone https://github.com/szpajder/libacars.git
 fi
@@ -69,33 +71,33 @@ echo ""
 
 ## BUILD AND INSTALL THE LIBACARS LIBRARY
 
-LogHeading "Building the libacars library"
+log_heading "Building the libacars library"
 
 if [[ ! -d $RECEIVER_BUILD_DIRECTORY/libacars/build ]]; then
-    LogMessage "Creating the libacars build directory"
+    log_message "Creating the libacars build directory"
     echo ""
         mkdir -vp $RECEIVER_BUILD_DIRECTORY/libacars/build
     echo ""
 fi
 if [[ -n "$(ls -A $RECEIVER_BUILD_DIRECTORY/libacars/build 2>/dev/null)" ]]
-    LogMessage "Deleting all files currently residing in the libacars build directory"
+    log_message "Deleting all files currently residing in the libacars build directory"
     rm -rf $RECEIVER_BUILD_DIRECTORY/libacars/build/*
 fi
-LogMessage "Entering the libacars build directory"
+log_message "Entering the libacars build directory"
 cd $RECEIVER_BUILD_DIRECTORY/libacars/build
-LogMessage "Executing cmake"
+log_message "Executing cmake"
 echo ""
 cmake ../
 echo ""
-LogMessage "Executing make"
+log_message "Executing make"
 echo ""
 make
 echo ""
-LogMessage "Executing make install"
+log_message "Executing make install"
 echo ""
 sudo make install
 echo ""
-LogMessage "Running ldconfig"
+log_message "Running ldconfig"
 echo ""
 sudo ldconfig
 echo ""
@@ -103,18 +105,18 @@ echo ""
 
 ## CLONE OR PULL THE ACARSDEC GIT REPOSITORY
 
-LogHeading "Preparing the ACARSDEC Git repository"
+log_heading "Preparing the ACARSDEC Git repository"
 
 if [[ -d $RECEIVER_BUILD_DIRECTORY/acarsdec && -d $RECEIVER_BUILD_DIRECTORY/acarsdec/.git ]]; then
-    LogMessage "Entering the ACARSDEC git repository directory"
+    log_message "Entering the ACARSDEC git repository directory"
     cd $RECEIVER_BUILD_DIRECTORY/acarsdec
-    LogMessage "Updating the local ACARSDEC git repository"
+    log_message "Updating the local ACARSDEC git repository"
     echo ""
     git pull
 else
-    LogMessage "Entering the ACARSDEC build directory"
+    log_message "Entering the ACARSDEC build directory"
     cd $RECEIVER_BUILD_DIRECTORY
-    LogMessage "Cloning the ACARSDEC git repository locally"
+    log_message "Cloning the ACARSDEC git repository locally"
     echo ""
     git clone https://github.com/TLeconte/acarsdec.git
 fi
@@ -123,32 +125,32 @@ echo ""
 
 ## BUILD AND INSTALL THE ACARSDEC BINARY
 
-LogHeading "Building the ACARSDEC binary"
+log_heading "Building the ACARSDEC binary"
 
 if [[ ! -d $RECEIVER_BUILD_DIRECTORY/acarsdec/build ]]; then
-    LogMessage "Creating the ACARSDEC build directory"
+    log_message "Creating the ACARSDEC build directory"
     echo ""
         mkdir -vp $RECEIVER_BUILD_DIRECTORY/acarsdec/build
     echo ""
 fi
 if [[ -n "$(ls -A $RECEIVER_BUILD_DIRECTORY/acarsdec/build 2>/dev/null)" ]]
-    LogMessage "Deleting all files currently residing in the ACARSDEC build directory"
+    log_message "Deleting all files currently residing in the ACARSDEC build directory"
     rm -rf $RECEIVER_BUILD_DIRECTORY/acarsdec/build/*
 fi
-LogMessage "Entering the ACARSDEC build directory"
+log_message "Entering the ACARSDEC build directory"
 cd $RECEIVER_BUILD_DIRECTORY/acarsdec/build
-LogMessage "Executing cmake"
+log_message "Executing cmake"
 echo ""
 
 # TODO: Choose the proper parameters depending on the chosen device
 
 cmake .. -Drtl=ON or -Dairspy=ON or -Dsdrplay=ON
 echo ""
-LogMessage "Executing make"
+log_message "Executing make"
 echo ""
 make
 echo ""
-LogMessage "Executing make install"
+log_message "Executing make install"
 echo ""
 sudo make install
 echo ""
@@ -158,12 +160,12 @@ echo ""
 
 ## SETUP COMPLETE
 
-LogMessage "Returning to ${RECEIVER_PROJECT_TITLE} root directory"
+log_message "Returning to ${RECEIVER_PROJECT_TITLE} root directory"
 cd $RECEIVER_ROOT_DIRECTORY
 
 echo ""
-LogTitleMessage "------------------------------------------------------------------------------"
-LogTitleHeading "ACARSDEC decoder setup is complete"
+log_title_message "------------------------------------------------------------------------------"
+log_title_heading "ACARSDEC decoder setup is complete"
 echo ""
 read -p "Press enter to continue..." discard
 
