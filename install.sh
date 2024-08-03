f97e468c3592ad9af8b0da94ac727d744f895463
Joseph Prochazka <joe@swiftbyte.com>
2024-08-02 22:18:38 -0400

    Bumped version to 2.8.6.

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@
 #                                                                                   #
 #  -------------------------------------------------------------------------------  #
 #                                                                                   #
-# Copyright (c) 2015 Joseph A. Prochazka                                            #
+# Copyright (c) 2015 Joseph A. Prochazka and Contributors                           #
 #                                                                                   #
 # Permission is hereby granted, free of charge, to any person obtaining a copy      #
 # of this software and associated documentation files (the "Software"), to deal     #
